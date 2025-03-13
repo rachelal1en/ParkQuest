93713cf9e5dbc5caa5d0cf84e4d33613517b2c38
Sara Bohannon <your.email@example.com>
2025-03-13 08:33:06 -0500

    parkEntity

---
+++ parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/ParkRepository.java
@@ -0,0 +1,9 @@
+package com.parkrangers.parkquest_backend;
+
+import com.parkrangers.parkquest_backend.models.Park;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParkRepository extends CrudRepository<Park, Integer> {
+}
--- parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/models/Park.java
+++ parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/models/Park.java
@@ -11,36 +11,33 @@ import java.util.List;
 @Entity
 public class Park extends AbstractEntity {
 
-    private String location;
-
-    @OneToMany(mappedBy = "park")
-    private List<Amenity> amenities = new ArrayList<>();
-
-    @OneToMany(mappedBy = "park")
-    private List<ContactInfo> contacts = new ArrayList<>();
-
-    @OneToMany(mappedBy = "park")
-    private List<OperatingHours> operatingHours = new ArrayList<>();
-
-    @OneToMany(mappedBy = "park")
-    private List<EntranceFee> entranceFees = new ArrayList<>();
-
+    private String parkId;
+    private String fullName;
+    private String states;
 
     public Park() {}
 
+    public String getParkId() {
+        return parkId;
+    }
 
-    public String getLocation() { return location; }
-    public void setLocation(String location) { this.location = location; }
+    public void setParkId(String parkId) {
+        this.parkId = parkId;
+    }
 
-    public List<Amenity> getAmenities() { return amenities; }
-    public void setAmenities(List<Amenity> amenities) { this.amenities = amenities; }
+    public String getFullName() {
+        return fullName;
+    }
 
-    public List<ContactInfo> getContacts() { return contacts; }
-    public void setContacts(List<ContactInfo> contacts) { this.contacts = contacts; }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 
-    public List<OperatingHours> getOperatingHours() { return operatingHours; }
-    public void setOperatingHours(List<OperatingHours> operatingHours) { this.operatingHours = operatingHours; }
+    public String getStates() {
+        return states;
+    }
 
-    public List<EntranceFee> getEntranceFees() { return entranceFees; }
-    public void setEntranceFees(List<EntranceFee> entranceFees) { this.entranceFees = entranceFees; }
+    public void setStates(String states) {
+        this.states = states;
+    }
 }
