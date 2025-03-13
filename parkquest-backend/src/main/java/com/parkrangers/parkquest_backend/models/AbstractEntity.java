1b27670ae5ad0ba73edf89694e58e3fb7d056f78
Sara Bohannon <your.email@example.com>
2025-03-13 11:55:31 -0500

    ParkRepository

--- parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/models/AbstractEntity.java
+++ parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/models/AbstractEntity.java
@@ -2,7 +2,7 @@ package com.parkrangers.parkquest_backend.models;
 
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.Id;
+
 import jakarta.persistence.MappedSuperclass;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
