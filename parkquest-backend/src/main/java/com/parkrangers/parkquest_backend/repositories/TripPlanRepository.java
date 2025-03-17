4d3c956be7fd6df4ebc0881e68829b6d1c0c4d68
Sara Bohannon <your.email@example.com>
2025-03-16 20:08:06 -0500

    still not fixed

--- parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/repositories/TripPlanRepository.java
+++ parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/repositories/TripPlanRepository.java
@@ -1,12 +1,4 @@
 package com.parkrangers.parkquest_backend.repositories;
 
-import com.parkrangers.parkquest_backend.models.TripPlan;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TripPlanRepository extends CrudRepository <TripPlan, Integer> {
+public interface TripPlanRepository {
 }
-
-
