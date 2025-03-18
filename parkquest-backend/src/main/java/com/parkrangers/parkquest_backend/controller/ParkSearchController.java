0a464f1a044ad2fd06b24ab9a47008f6b3678959
sheetalkkharat <sheetalkharatk@gmail.com>
2025-03-17 20:52:23 -0500

    ParkSearchController

---
+++ parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/controller/ParkSearchController.java
@@ -0,0 +1,19 @@
+package com.parkrangers.parkquest_backend.controller;
+
+import com.parkrangers.parkquest_backend.models.response.Park;
+import com.parkrangers.parkquest_backend.service.ParkSearchService;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ParkSearchController {
+
+    private ParkSearchService parkSearchService;
+
+    @GetMapping("/parks/searches")
+    public List<Park> getParks(@RequestParam String stateCode, @RequestParam String parkCode) {
+        return parkSearchService.getParks(stateCode, parkCode);
+    }
+}
