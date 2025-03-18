670a146236574a904dcc67467f24e08a39bc9583
sheetalkkharat <sheetalkharatk@gmail.com>
2025-03-17 21:11:06 -0500

    Add ParkSearchResponse

--- parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/response/ParkSearchResponse.java
+++ parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/response/ParkSearchResponse.java
@@ -1,4 +1,16 @@
-package com.parkrangers.parkquest_backend.response;
+package com.parkrangers.parkquest_backend.models.response;
+
+import java.util.List;
 
 public class ParkSearchResponse {
+
+    private List<Park> data;
+
+    public List<Park> getData() {
+        return data;
+    }
+
+    public void setData(List<Park> data) {
+        this.data = data;
+    }
 }
