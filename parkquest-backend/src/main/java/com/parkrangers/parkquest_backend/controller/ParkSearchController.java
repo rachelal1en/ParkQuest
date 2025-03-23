e6c99afb153739b4b43b8a328ddd6222f4226951
sheetalkkharat <sheetalkharatk@gmail.com>
2025-03-22 20:02:39 -0500

    Changes in search controller

--- parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/controller/ParkSearchController.java
+++ parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/controller/ParkSearchController.java
@@ -3,6 +3,8 @@ package com.parkrangers.parkquest_backend.controller;
 import com.parkrangers.parkquest_backend.models.response.Park;
 import com.parkrangers.parkquest_backend.service.ParkSearchService;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ParkSearchController {
 
+    @Autowired
     private ParkSearchService parkSearchService;
 
     @GetMapping("/parks/searches")
