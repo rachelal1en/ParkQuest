7aa0734c22aeae315686e2e37cfc54371d7b2095
sheetalkkharat <sheetalkharatk@gmail.com>
2025-03-22 22:01:08 -0500

    Added CorsConfig file for CORS configuration

---
+++ parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.parkrangers.parkquest_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**") // Apply to all endpoints in the application
+                .allowedOrigins("http://localhost:5173/") // Allow CORS for these origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow specific HTTP methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(true); // Allow credentials such as cookies or HTTP authentication
+    }
+}
