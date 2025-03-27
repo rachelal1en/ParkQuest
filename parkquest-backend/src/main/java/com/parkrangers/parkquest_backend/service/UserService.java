d0cdff97babf37da6074860126a03c0fa6e8d0cd
sheetalkkharat <sheetalkharatk@gmail.com>
2025-03-27 18:55:34 -0500

    UserService.java

--- parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/service/UserService.java
+++ parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/service/UserService.java
@@ -8,16 +8,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
-    // Retrieve the authenticated user from the SecurityContext
+
     public User getAuthenticatedUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication != null && authentication.isAuthenticated()) {
-            // Assuming the user details are stored in the principal object
+
             User user = (User) authentication.getPrincipal();
             return user;
         } else {
-            // Handle the case where there is no authenticated user (e.g., throw an exception or return a default user)
+
             throw new RuntimeException("No authenticated user found");
         }
     }
