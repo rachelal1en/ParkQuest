3d09e2c7244f7d2dd4969cb11d9ededa081ad326
Sara Bohannon <your.email@example.com>
2025-04-12 22:19:44 -0500

    subscription working, favorite first

---
+++ parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/model/request/SubscriptionRequest.java
@@ -0,0 +1,4 @@
+package com.parkrangers.parkquest_backend.model.request;
+
+public class SubscriptionRequest {
+}
---
+++ parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/model/response/Subscription.java
@@ -0,0 +1,4 @@
+package com.parkrangers.parkquest_backend.model.response;
+
+public class Subscription {
+}
