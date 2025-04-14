14d9210743d06640f70659524c2af2aa3aec25b4
Rachel Allen <rqllen@gmail.com>
2025-04-13 21:02:01 -0500

    events and subscriptions

--- parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/repository/SubscriptionRepository.java
+++ parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/repository/SubscriptionRepository.java
@@ -1,4 +1,13 @@
 package com.parkrangers.parkquest_backend.repository;
 
-public class SubscriptionRepository {
+import com.parkrangers.parkquest_backend.model.User;
+import com.parkrangers.parkquest_backend.model.response.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+    List<Subscription> findByUser(User user);
+    Optional<Subscription> findByUserAndParkCode(User user, String parkCode);
 }
--- parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/service/EventService.java
+++ parkquest-backend/src/main/java/com/parkrangers/parkquest_backend/service/EventService.java
@@ -1,4 +1,38 @@
 package com.parkrangers.parkquest_backend.service;
 
+import com.parkrangers.parkquest_backend.model.response.Event;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
 public class EventService {
+
+    @Value("${api.key}")
+    private String apiKey;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public List<Event> fetchEventsByParkCode(String parkCode) {
+        String url = "https://developer.nps.gov/api/v1/events?parkCode=" + parkCode + "&api_key=" + apiKey;
+
+        try {
+            NpsEventResponse response = restTemplate.getForObject(url, NpsEventResponse.class);
+            return response != null ? response.getData() : List.of();
+        } catch (Exception e) {
+            System.out.println(" Error fetching events: " + e.getMessage());
+            return List.of();
+        }
+    }
+
+
+    public static class NpsEventResponse {
+        private List<Event> data;
+
+        public List<Event> getData() { return data; }
+        public void setData(List<Event> data) { this.data = data; }
+    }
 }
